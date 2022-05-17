8426f068d354d6d78ee373cf24d84fa37017a977
Luke McDonald <luke1a.mcd+git@gmail.com>
2022-05-17 10:43:20 +1000

    h

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+namespace temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int,int> Fib = null;
+            Fib = (x) => x > 1 ? Fib(x-1) + Fib(x-2) : x;
+            Console.WriteLine(Fib(1));
+        }
+    }
+}
